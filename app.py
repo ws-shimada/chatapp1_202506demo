463f9cf910fb4bc3cbd755da69e558cb4f30403d
ws-shimada <w9j9i24zt@gmail.com>
2025-06-04 15:35:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,14 +44,10 @@ def input_id():
     if not "user_id" in st.session_state:
         st.session_state.user_id = "hogehoge"
     with st.form("id_form", enter_to_submit=False):
-        user_id = st.text_input('学籍番号を入力し、送信ボタンを押してください')
-        submit_id = st.form_submit_button(
-            label="送信",
-            type="primary")
         prompt_option = st.selectbox(
             "プロンプト選択※テスト用フォーム",
             ("{}".format(prompt_list[0]), "{}".format(prompt_list[1])),)
-        user_id = st.text_input('学籍番号を入力し、送信ボタンを押してください')
+        user_id = st.text_input('学籍番号を入力し、送信ボタンを押してください', key="user_id_input")
         submit_id = st.form_submit_button(
             label="送信",
             type="primary")
